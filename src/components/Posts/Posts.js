81655ba1c86b2021e26cef87504524f1f2827d2f
Juneroy Quinimon <juneroyquinimon@ocomsoft.com>
2022-04-16 01:57:29 +0800

    display list of memories add other dependencies

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -4,19 +4,26 @@ import { useSelector } from "react-redux";
 import Post from "./Post/Post";
 import useStyles from './styles'
 
+import { Grid, CircularProgress } from "@material-ui/core";
+
 const Posts = () => {
     const posts = useSelector((state) => state.posts);
     const classes = useStyles();
     console.log(posts);
     return (
-        <>
-            <h1>
-                Posts
-            </h1>
-            <Post />
-            <Post />
-        </>
-    )
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                {
+                    posts.map((post, index) => (
+                        <Grid key={index} item xs={12} sm={6}>
+                            <Post post={post} />
+                        </Grid>
+                    ))
+                }
+            </Grid>
+
+        )
+    );
 }
 
 export default Posts;

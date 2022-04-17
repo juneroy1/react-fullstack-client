ceda9f71151783577340145dc3a9e2f43e072e23
Juneroy Quinimon <juneroyquinimon@ocomsoft.com>
2022-04-17 16:15:32 +0800

    add auth js file

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core'
+// import Lock
+import useStyles from './styles'
 const Auth = () => {
+    const classes = useStyles();
   return (
-    <div>Auth</div>
+    <Container component="main" maxWidth="xs">
+        <Paper className={classes.paper} elevation={3}>
+            <Avatar className={classes.avatar}>
+                {/* <LockOut */}
+            </Avatar>
+        </Paper>
+    </Container>
   )
 }
 

423ddaaf0df5141fa8b35ae3fcf3f04545eb17b1
Juneroy Quinimon <juneroyquinimon@ocomsoft.com>
2022-04-17 15:12:53 +0800

    fixed mobile responsive

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -7,7 +7,7 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 import {useDispatch} from 'react-redux';
-import { deletePost } from "../../../actions/posts";
+import { deletePost, likePost } from "../../../actions/posts";
 import moment from 'moment'
 
 const Post = ({ post, setCurrentId }) => {
@@ -34,12 +34,12 @@ const Post = ({ post, setCurrentId }) => {
             <Typography className={classes.title} variant="h5" gutterBottom>{post.title}</Typography>
 
             <CardContent>
-            <Typography  variant="h5" gutterBottom>{post.message}</Typography>
+            <Typography  variant="body2" color="textSecondary" component="p" gutterBottom>{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={()=>{}}>
-                    <ThumbUpAltIcon fontSize="small"/>
-                    Like
+                <Button size="small" color="primary" onClick={()=>{dispatch(likePost(post._id))}}>
+                    <ThumbUpAltIcon fontSize="small"/> &nbsp;
+                    Like &nbsp;
                     {post.likeCount}
                 </Button>
                 <Button size="small" color="primary" onClick={()=>{dispatch(deletePost(post._id))}}>
